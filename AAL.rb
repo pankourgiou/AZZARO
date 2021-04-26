310115a79413ae1b4a718e71d582a85da972eaf3
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-04-26 22:05:59 +0300

    Update AAL.rb

--- AAL.rb
+++ AAL.rb
@@ -1,3 +1,8 @@
-10000000000000000000000000.times do
-     puts "Are you ill of innumeracy??? Don't worry Animals as leaders are here..."
-end
+phi = c(rep(0,20),.8)
+ACF = ARMAacf(ar=phi, ma=-.5, 80)[-1]  
+PACF = ARMAacf(ar=phi, ma=-.5, 80, pacf=TRUE) 
+par(mfrow=c(2,4))
+plot(ACF,  type="h", xlab="lag", ylim=c(-.4,.8))
+abline(h=0)
+plot(PACF, type="h", xlab="lag", ylim=c(-.4,.8))
+abline(h=0)
