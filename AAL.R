84df25718d9c45a89c27b723647a5cbbaf01c6a7
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 05:28:49 +0300

    Update and rename AAL.R to Socrates.py

--- AAL.R
+++
@@ -1,8 +0,0 @@
-phi = c(rep(0,20),.8)
-ACF = ARMAacf(ar=phi, ma=-.5, 80)[-1]  
-PACF = ARMAacf(ar=phi, ma=-.5, 80, pacf=TRUE) 
-par(mfrow=c(2,4))
-plot(ACF,  type="h", xlab="lag", ylim=c(-.4,.8))
-abline(h=0)
-plot(PACF, type="h", xlab="lag", ylim=c(-.4,.8))
-abline(h=0)
---
+++ Socrates.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("The unexamined life is not worth living")
