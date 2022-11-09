9ed1118acea3cd9318991d9f1774e9c6ab7dbd7b
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-11-09 04:56:50 +0200

    Update and rename Azaro.py to Hello3.cs

---
+++ Hello3.cs
@@ -0,0 +1,3 @@
+//We should watch more tennis..or listen to Iron Maiden low profile albums.
+System.Console.WriteLine("French word Racket(which is a programming language) means black mailing, it's strange doucette means sweetness and it's not a number...module has no Greek Translation plus chinese people eat banana apple..it's one fruit not two. You are Somewhere in time...:)Backhand!!!");
+
